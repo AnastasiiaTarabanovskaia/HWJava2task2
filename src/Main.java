53ad4d3b6aebe0246cbf21964c487c68642177f7
Anastasiia T <anastasiiatarabanovskaia@gmai.com>
2025-03-04 22:20:59 +0300

    src changed

--- src/Main.java
+++ src/Main.java
@@ -2,13 +2,12 @@ public class Main {
     public static void main(String[] args) {
         int initialAmount = 200;
         int addedAmount = 1000;
-        int bonusPerHundredRubles = 1;
-        int x = addedAmount / 100 * 1 + initialAmount + addedAmount;
+        int finalBalanceAndBonus = addedAmount / 100 * 1 + initialAmount + addedAmount;
         if (addedAmount >= 1000) {
-            System.out.println(" Итоговый баланс " + x + " Количество бонусов " + addedAmount / 100 );
+            System.out.println(" Итоговый баланс " + finalBalanceAndBonus + " Количество бонусов " + addedAmount / 100 );
         } else {
-            int y = initialAmount + addedAmount;
-            System.out.println(" Итоговый баланс " + y + " Бонусов нет ");
+            int finalBalanceNoBonus = initialAmount + addedAmount;
+            System.out.println(" Итоговый баланс " + finalBalanceNoBonus + " Бонусов нет ");
         }
 
     }
